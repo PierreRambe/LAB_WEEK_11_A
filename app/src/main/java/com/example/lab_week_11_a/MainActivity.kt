2a95d37b6cae4c333e4e2cfd53dc9ba742adce7b
pierre <prambe18@gmail.com>
2025-11-24 21:36:52 +0700

    Commit No. 1: Building App with SharedPreferences and DataStore

--- app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
@@ -14,22 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val preferenceWrapper = (application as PreferenceApplication).preferenceWrapper
+        val settingsStore = (application as SettingsApplication).settingsStore
 
-        val preferenceViewModel =
+        val settingsViewModel =
             ViewModelProvider(this, object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                    return PreferenceViewModel(preferenceWrapper) as T
+                    return SettingsViewModel(settingsStore) as T
                 }
-            })[PreferenceViewModel::class.java]
+            })[SettingsViewModel::class.java]
 
-        preferenceViewModel.getText().observe(this) {
+        settingsViewModel.textLiveData.observe(this) {
             findViewById<TextView>(R.id.activity_main_text_view).text = it
         }
 
         findViewById<Button>(R.id.activity_main_button).setOnClickListener {
             val input = findViewById<EditText>(R.id.activity_main_edit_text).text.toString()
-            preferenceViewModel.saveText(input)
+            settingsViewModel.saveText(input)
         }
     }
 }
